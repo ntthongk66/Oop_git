494545d303310108848f8d787d20b38ca7834360
ntthong <21020408@vnu.edu.vn>
2022-09-06 10:11:33 +0700

    added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static String printHello() {
+        return "HelloWorld";
+    }
+
+    public static void main(String[] args) {
+        System.out.print(printHello());
+    }
+}
